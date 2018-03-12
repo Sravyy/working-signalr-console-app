a6ba912fb369d8ba8384ecd3652bb53bc35b8788
sravanthi velaga <svelaga0412@gmail.com>
2018-03-12 01:05:28 -0700

    initial commit

---
+++ Client/SingnalRClient/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingnalRClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new HubConnection("http://localhost:8080/");
+            var myHub = connection.CreateHubProxy("MyHub");
+
+            connection.Start().ContinueWith(task => {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }).Wait();
+
+            myHub.Invoke<string>("Send", "HELLO World ").ContinueWith(task => {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error calling send: {0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine(task.Result);
+                }
+            });
+
+            myHub.On<string>("addMessage", param => {
+                Console.WriteLine(param);
+            });
+
+            myHub.Invoke<string>("DoSomething", "I'm doing something!!!").Wait();
+
+
+            Console.Read();
+            connection.Stop();
+
+        }
+    }
+}
---
+++ SignalRServer/MyHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRServer
+{
+    [HubName("MyHub")]
+    public class MyHub : Hub
+    {
+        public string Send(string message)
+        {
+            Console.WriteLine("Recieved: " + message);
+            return message;
+        }
+
+        public void DoSomething(string param)
+        {
+            Console.WriteLine("Recieved: " + param);
+            Clients.All.addMessage(param);
+        }
+    }
+}
